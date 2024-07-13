9a4389d55271e3fc6081184787bc050e7d927e07
EzequielMonson <ezemonson2024@gmail.com>
2024-07-12 22:09:13 -0300

    modifique la ruta

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import json
 
 
 def traer_data_JSON():
-    file_path = '/api/app/data.json'
+    file_path = 'app\data.json'
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
